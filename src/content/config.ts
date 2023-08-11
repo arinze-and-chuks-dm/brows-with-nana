07a8028ae606ee0c01c0f38d7d1321979c3a9e1b
Chuks1993 <chuksgrinage.dev@gmail.com>
2023-08-11 00:23:19 -0500

    faq

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,22 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from "astro:content"
 
 // Pages collection schema
 const pagesCollection = defineCollection({
-  schema: z.object({
+  schema: ({ image }) => z.object({
     id: z.string().optional(),
     title: z.string(),
+    subtitle: z.string().optional(),
     meta_title: z.string().optional(),
     description: z.string().optional(),
-    image: z.string().optional(),
+    image: z.object({
+      src: image().refine((img ) => img.width >= 600, {
+        message: "Image must be at least 600px wide",
+      }),
+      alt: z.string(),
+    }),
     layout: z.string().optional(),
     draft: z.boolean().optional(),
+    faq: z.array(z.object({ question: z.string(), answer: z.array(z.string()) })).optional()
   }),
 });
 
