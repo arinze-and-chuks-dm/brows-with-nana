0ea50ba242a2cdbf7f5e71a34acd1cbdbf83dcc7
Chuks1993 <chuksgrinage.dev@gmail.com>
2023-08-19 15:38:35 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
-import config from "./src/config/config.json";
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
+import config from "./src/config/config.json"
 
-import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
@@ -24,8 +22,6 @@ export default defineConfig({
     // image({
     //   serviceEntryPoint: "@astrojs/image/sharp",
     // }),
-    mdx(),
-    react(),
   ],
   markdown: {
     remarkPlugins: [],
